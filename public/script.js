97ac51cc850b658dd1037f6c3510bb888b13eff7
Kalsef <second987i@gmail.com>
2025-09-02 21:52:26 -0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -356,19 +356,23 @@ carregarMusicas();
 function addSecondStepEvents() {
   document.getElementById('yesFinalBtn').addEventListener('click', async () => {
     try {
+      // Faz requisição POST para seu endpoint do backend
       const res = await fetch("/api/send-delete-alert", { method: "POST" });
       const data = await res.json();
+
       if (data.success) {
         alert("Ação confirmada e notificação enviada no Discord!");
       } else {
         alert("Erro ao enviar notificação.");
         console.error("Erro:", data.error);
       }
+
     } catch (err) {
       console.error(err);
       alert("Erro ao enviar notificação.");
     }
 
+    // Fecha o modal
     modal.classList.remove('show');
   });
 
